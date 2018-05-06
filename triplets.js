df9b7d5d2b301bc46dcc5927f63f9245e9738124
Manuel Pichardo <mlpichardo1@gmail.com>
2018-05-06 21:59:33 +0000

    triplets completed

---
+++ triplets.js
@@ -0,0 +1,35 @@
+function solve(a0, a1, a2, b0, b1, b2) 
+{
+    var alice = 0;
+    var bob = 0;
+    
+    if (a0 > b0)
+    {
+        alice++;
+    }
+        else if (a0 < b0)
+        {
+        bob++;
+        }
+   
+    if (a1 > b1)
+    {
+        alice++;
+    }
+        else if (a1 < b1)
+        {
+        bob++;
+        }
+    
+    if (a2 > b2)
+    {
+        alice++;
+    }
+        else if (a2 < b2)
+        {
+        bob++;
+        }
+    
+ return [alice, bob];   
+    
+}
