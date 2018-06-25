b6199170381b8a4a4cbf7cc36f2d2d526f582149
Manuel Pichardo <mlpichardo1@gmail.com>
2018-06-25 22:04:30 +0000

    started apples and organges algorithm

---
+++ appleOrange.js
@@ -0,0 +1,11 @@
+// Complete the countApplesAndOranges function below.
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    var applesfell = 0;
+    var orangesfell = 0;
+    var home = [s, t];
+    
+    if (s <= home <= t) {
+        return home;
+    }
+
+}
